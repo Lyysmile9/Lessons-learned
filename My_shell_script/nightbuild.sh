05f18c4e2ca630a1a1187826a278b39ced0ea442
jinfa.huang <jinfa.huang@zongmutech.com>
2020-02-26 17:52:17 +0800

    nightbuid: rebuild code
    Signed-off-by: jinfa.huang <jinfa.huang@zongmutech.com>

--- My_shell_script/nightbuild.sh
+++ My_shell_script/nightbuild.sh
@@ -2,46 +2,89 @@
 
 export PATH=/home/xm/AHD_BSP/main/poky/sources/linux-x86/toolchain/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux/bin:$PATH
 
-dir=(/home/xm/zadas)
+dir=(/home/xm/zadas /home/xm/ZM_A7_AHD_GENERIC)
 
-#	for i in 0 1
-#	do
-currentdir=${dir[i]}
-cd $currentdir
-echo "currentdir:"$currentdir
-#git branch
-git log --abbrev --oneline -1 > /home/xm/HEAD
+get_old_commit()
+{
+	git log --abbrev --oneline -1 > /home/xm/HEAD
+	c=$(cat /home/xm/HEAD)
+	OldCommit=$(echo ${c:0:6})
+	echo "Old commot: $OldCommit"
+}
 
-cat /home/xm/HEAD | while read -r -a word;
-	do
-		echo "current commit: $word"
-		git reset --hard $word
-		git fetch origin zm_develop_ahd_2.0_dev
-		git reset --hard origin/zm_develop_ahd_2.0_dev
-		git log --abbrev --oneline -1 > /home/xm/gitpull
-		cat /home/xm/gitpull |  while read -r -a words;
-		do
-			counterf=$(ls -l $currentdir/out/backup/ | grep "^d" | wc -l);
-			echo $counterf
+get_new_commit()
+{
+	git log --abbrev --oneline -1 > /home/xm/gitpull
+	c=$(cat /home/xm/gitpull)
+	NewCommit=$(echo ${c:0:6})	 
+	echo "New commit: $NewCommit"
+}
 
-			if [[ "$word" != "$words" ]] || [[ "$counterf" -eq 0 ]]; then
-#				git reset --hard $words
-#				rm -rf /home/xm/AHD_ZADAS/out/linux_atlas7 
-				make SKIP_CODING_STYLE_CHECK=1
-				mv $currentdir/out/linux_atlas7 $currentdir/out/backup/linux_atlas7_$words
-				elif [[ "$word" == "$words" ]]; then
-				echo "Is up to date..."
-			fi
+get_backup_file_number()
+{
+	counterf=$(ls -l $CurrentDir/out/backup/ | grep "^d" | wc -l)
+	echo "The number of files in the $CurrentDir/out/backup is: $counterf)"
+}
 
-		rm -rf /home/xm/HEAD /home/xm/gitpull
+clean_up_local_environment()
+{
+	git reset --hard $OldCommit
+}
 
-#git pull origin zm_ahd_2.0_dev
+update_code()
+{
+	if [ $i -eq 0 ]; then
+		branch=zm_develop_ahd_2.0_dev
+	else
+		branch=master
+	fi
+	
+	git fetch origin $branch
+	git reset --hard origin/$branch
+}
 
-		if [[ "$counterf" -gt "1" ]]; then
-			echo "do remove modify time greater than 7 days directory"
-			find $currentdir/out/backup/ -mtime +7 -type d | xargs rm -rf
-		fi
+remove_process_file()
+{
+	echo "remove process file"
+	rm -rf /home/xm/HEAD /home/xm/gitpull
+}
 
-		done
-	done
+clean_out_old_files()
+{
+	if [[ "$counterf" -gt "1" ]]; then
+		echo "do remove modify time greater than 7 days directory"
+		find $CurrentDir/out/backup/ -mtime +7 -type d | xargs rm -rf
+	fi
+
+}
+
+do_make()
+{
+for i in 0 1
+do
+	CurrentDir=${dir[i]}
+	cd $CurrentDir
+	echo "currentdir:"$CurrentDir
+
+	get_old_commit
+	clean_up_local_environment
+	update_code
+	get_new_commit
+	get_backup_file_number	
+
+	if [[ "$OldCommit" != "$NewCommit" ]] || [[ "$counterf" -eq 0 ]]; then
+		make SKIP_CODING_STYLE_CHECK=1
+		mv $CurrentDir/out/linux_atlas7 $CurrentDir/out/backup/linux_atlas7_$NewCommit
+	elif [[ "$OldCommit" == "$NewCommit" ]]; then
+		echo "Is up to date."
+	fi
+
+	remove_process_file
+	clean_out_old_files
+done
+}
+	
+do_make
+
 exit 0
+
